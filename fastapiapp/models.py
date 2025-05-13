d892e6b27c26ace6c3f57a490f4a30f48f34c5f2
Johnny Cheng <itainan.101@gmail.com>
2025-05-14 02:00:06 +0900

    remove unsued tables
    Signed-off-by: Johnny Cheng <itainan.101@gmail.com>

--- fastapiapp/models.py
+++ fastapiapp/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy import Index
 from sqlalchemy.schema import UniqueConstraint, PrimaryKeyConstraint
 from sqlalchemy.dialects.mysql import LONGTEXT, BIGINT, TIMESTAMP
@@ -34,89 +34,89 @@ class Video(Base):
     # captions = relationship("Caption", back_populates="video")
 
 
-class User(Base):
-    __tablename__ = "users"
-    ID = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
-    user_nicename = Column(String(50))
-    user_email = Column(String(100), unique=True, index=True)
-    user_pass = Column(String(255))
-    user_registered = Column(TIMESTAMP, server_default=func.now())
-    user_activation_key = Column(String(255))
+# class User(Base):
+#     __tablename__ = "users"
+#     ID = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
+#     user_nicename = Column(String(50))
+#     user_email = Column(String(100), unique=True, index=True)
+#     user_pass = Column(String(255))
+#     user_registered = Column(TIMESTAMP, server_default=func.now())
+#     user_activation_key = Column(String(255))
 
 
-class UserMeta(Base):
-    __tablename__ = "usermeta"
-    umeta_id = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
-    user_id = Column(BIGINT(unsigned=True), index=True)
-    meta_key = Column(String(255))
-    meta_value = Column(LONGTEXT)
+# class UserMeta(Base):
+#     __tablename__ = "usermeta"
+#     umeta_id = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
+#     user_id = Column(BIGINT(unsigned=True), index=True)
+#     meta_key = Column(String(255))
+#     meta_value = Column(LONGTEXT)
 
-    __table_args__ = (Index("meta_key_id", "meta_key", mysql_length=191),)
-
-
-class Post(Base):
-    __tablename__ = "posts"
-    ID = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
-    post_title = Column(Text)
-    post_author = Column(BIGINT, default=0, index=True)
-    post_content = Column(LONGTEXT)
-    post_parent = Column(BIGINT, index=True)
-    post_name = Column(String(200))
-    post_type = Column(String(20), default="post")
-    post_status = Column(String(20), default="publish")
-    created_date = Column(TIMESTAMP, server_default=func.now())
-    modified_date = Column(TIMESTAMP, onupdate=func.now())
-
-    __table_args__ = (
-        Index("post_name_idx", "post_name", mysql_length=191),
-        Index("post_type_idx", "post_type", "post_status", "created_date", "ID"),
-    )
+#     __table_args__ = (Index("meta_key_id", "meta_key", mysql_length=191),)
 
 
-class PostMeta(Base):
-    __tablename__ = "postmeta"
-    meta_id = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
-    post_id = Column(BIGINT(unsigned=True), default=0, index=True)
-    meta_key = Column(String(255))
-    meta_value = Column(LONGTEXT)
+# class Post(Base):
+#     __tablename__ = "posts"
+#     ID = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
+#     post_title = Column(Text)
+#     post_author = Column(BIGINT, default=0, index=True)
+#     post_content = Column(LONGTEXT)
+#     post_parent = Column(BIGINT, index=True)
+#     post_name = Column(String(200))
+#     post_type = Column(String(20), default="post")
+#     post_status = Column(String(20), default="publish")
+#     created_date = Column(TIMESTAMP, server_default=func.now())
+#     modified_date = Column(TIMESTAMP, onupdate=func.now())
 
-    __table_args__ = (Index("meta_key_id", "meta_key", mysql_length=191),)
-
-
-class Term(Base):
-    __tablename__ = "terms"
-    term_id = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
-    name = Column(String(200))
-    slug = Column(String(200))
-
-    __table_args__ = (
-        Index("name", "name", mysql_length=191),
-        Index("slug", "slug", mysql_length=191),
-    )
+#     __table_args__ = (
+#         Index("post_name_idx", "post_name", mysql_length=191),
+#         Index("post_type_idx", "post_type", "post_status", "created_date", "ID"),
+#     )
 
 
-class TermRelationship(Base):
-    __tablename__ = "term_relationships"
-    object_id = Column(BIGINT(unsigned=True))
-    term_taxonomy_id = Column(BIGINT(unsigned=True), index=True)
-    term_order = Column(Integer)
+# class PostMeta(Base):
+#     __tablename__ = "postmeta"
+#     meta_id = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
+#     post_id = Column(BIGINT(unsigned=True), default=0, index=True)
+#     meta_key = Column(String(255))
+#     meta_value = Column(LONGTEXT)
 
-    __table_args__ = (PrimaryKeyConstraint("object_id", "term_taxonomy_id"),)
+#     __table_args__ = (Index("meta_key_id", "meta_key", mysql_length=191),)
 
 
-class TermTaxonomy(Base):
-    __tablename__ = "term_taxonomy"
-    term_taxonomy_id = Column(
-        BIGINT(unsigned=True), primary_key=True, autoincrement=True
-    )
-    term_id = Column(BIGINT(unsigned=True), index=True)
-    taxonomy = Column(String(32))
-    description = Column(LONGTEXT)
-    parent = Column(BIGINT(unsigned=True))
-    count = Column(BIGINT)
+# class Term(Base):
+#     __tablename__ = "terms"
+#     term_id = Column(BIGINT(unsigned=True), primary_key=True, autoincrement=True)
+#     name = Column(String(200))
+#     slug = Column(String(200))
 
-    __table_args__ = (
-        Index("term_id_taxonomy_id", "term_id", "taxonomy"),
-        Index("taxonomy_id", "taxonomy"),
-        UniqueConstraint("term_id", "taxonomy"),
-    )
+#     __table_args__ = (
+#         Index("name", "name", mysql_length=191),
+#         Index("slug", "slug", mysql_length=191),
+#     )
+
+
+# class TermRelationship(Base):
+#     __tablename__ = "term_relationships"
+#     object_id = Column(BIGINT(unsigned=True))
+#     term_taxonomy_id = Column(BIGINT(unsigned=True), index=True)
+#     term_order = Column(Integer)
+
+#     __table_args__ = (PrimaryKeyConstraint("object_id", "term_taxonomy_id"),)
+
+
+# class TermTaxonomy(Base):
+#     __tablename__ = "term_taxonomy"
+#     term_taxonomy_id = Column(
+#         BIGINT(unsigned=True), primary_key=True, autoincrement=True
+#     )
+#     term_id = Column(BIGINT(unsigned=True), index=True)
+#     taxonomy = Column(String(32))
+#     description = Column(LONGTEXT)
+#     parent = Column(BIGINT(unsigned=True))
+#     count = Column(BIGINT)
+
+#     __table_args__ = (
+#         Index("term_id_taxonomy_id", "term_id", "taxonomy"),
+#         Index("taxonomy_id", "taxonomy"),
+#         UniqueConstraint("term_id", "taxonomy"),
+#     )
